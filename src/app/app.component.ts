090195738829a11b863db0bfa84d8723577d6f3e
marciano <marciano@debian>
2021-05-25 10:42:53 -0300

    subindo projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServicoPrincipalService } from './servico-principal.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pet-shop-maria';
+
+  eventoBotao: any = "dono";
+  
+  constructor(private service: ServicoPrincipalService) { 
+  
+  }
+
+  ngOnInit() {
+  }
+
+  eventoBotaService(evento: String){
+    this.eventoBotao = evento;
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VacinaComponent } from './vacina/vacina.component';
+import { BanhoComponent } from './banho/banho.component';
+import { ServicoPrincipalService } from './servico-principal.service';
+import { AnimalComponent } from './animal/animal.component';
+import { DonoComponent } from './dono/dono.component';
+import { CaixaComponent } from './caixa/caixa.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VacinaComponent,
+    BanhoComponent,
+    AnimalComponent,
+    DonoComponent,
+    CaixaComponent,
+    ProdutoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [ServicoPrincipalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
